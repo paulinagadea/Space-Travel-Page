9686f76cf7fa8ee507700e83a6075b6d5cc8c17c
Paulina Gadea <paulinagadea2702@gmail.com>
2022-12-03 16:01:25 -0300

    home css done

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,8 +6,9 @@ export default function Home() {
     return (
         <div className={styles.background}>
             <DesktopNavBar />
-
-            <div>
+            
+            <div className={styles.content}>
+            <div className={styles.container}>
                 <h3>So, you want to travel to</h3>
                 <h1>Space</h1>
                 <p>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. 
@@ -15,8 +16,9 @@ export default function Home() {
             </div>
 
             <Link to="/destination">
-                <button>Explore</button>
+                <button className={styles.button}>Explore</button>
             </Link>
+            </div>
         </div>
     )
 };
--- src/components/navbar/DesktopNavBar.jsx
+++ src/components/navbar/DesktopNavBar.jsx
@@ -1,22 +1,27 @@
 import { Link } from "react-router-dom";
+import styles from "./NavBar.module.css";
 
 export default function DesktopNavBar() {
     return (
-        <nav>
-            <ul>
+        <div className={styles.navBar}>
+            <img src="/assets/shared/logo.svg" alt="logo" className={styles.logo}/>
+            <span className={styles.line}></span>
+        <nav className={styles.nav}>
+            <ul className={styles.content}>
                 <Link to="/">
-                    <li>00 Home</li>
+                    <li><span>00</span> Home</li>
                 </Link>
                 <Link to="/destination">
-                    <li>01 Destination</li>
+                    <li><span>01</span> Destination</li>
                 </Link>
                 <Link to="/crew">
-                    <li>02 Crew</li>
+                    <li><span>02</span> Crew</li>
                 </Link>
                 <Link to="/technology">
-                    <li>03 Technology</li>
+                    <li><span>03</span> Technology</li>
                 </Link>
             </ul>
         </nav>
+        </div>
     )
 };
