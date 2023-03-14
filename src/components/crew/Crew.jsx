b51c8e9a9ca335f52fa3bed8ee55cf46626777a5
paulinagadea <paulinagadea2702@gmail.com>
2023-03-14 18:37:13 -0300

    crew

--- src/components/crew/Crew.jsx
+++ src/components/crew/Crew.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import CrewData from "./CrewData.jsx";
+import styles from "./Crew.module.css";
 import DesktopNavBar from "../navbar/DesktopNavBar.jsx";
 
 export default function Crew() {
@@ -7,35 +8,39 @@ export default function Crew() {
     const { data } = CrewData(currentContent);
 
     return (
-        <div>
+        <div className={styles.back}>
             <DesktopNavBar />
 
-            <h4>02 Meet your crew</h4>
+            <div className={styles.mainContainer}>
 
-            <div>
-                <h3>{data.role}</h3>
-                <h1>{data.name}</h1>
-                <p>{data.bio}</p>
+                <div className={styles.crewContainer}>
+                    <h4><span>02</span> Meet your crew</h4>
+                    <h3>{data.role}</h3>
+                    <h1>{data.name}</h1>
+                    <p>{data.bio}</p>
+
+                    <nav>
+                        <ul>
+                            <li>
+                                <button onClick={() => setCurrentContent(1)}></button>
+                            </li>
+                            <li>
+                                <button onClick={() => setCurrentContent(2)}></button>
+                            </li>
+                            <li>
+                                <button onClick={() => setCurrentContent(3)}></button>
+                            </li>
+                            <li>
+                                <button onClick={() => setCurrentContent(4)}></button>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div className={styles.imgBox}>
+                    <img src={data.image} alt={data.name} />
+                </div>
             </div>
-
-            <nav>
-                <ul>
-                    <li>
-                        <button onClick={() => setCurrentContent(1)}></button>
-                    </li>
-                    <li>
-                        <button onClick={() => setCurrentContent(2)}></button>
-                    </li>
-                    <li>
-                        <button onClick={() => setCurrentContent(3)}></button>
-                    </li>
-                    <li>
-                        <button onClick={() => setCurrentContent(4)}></button>
-                    </li>
-                </ul>
-            </nav>
-
-            <img src={data.image} alt={data.name} />
         </div>
     )
 };
